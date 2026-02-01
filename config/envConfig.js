58bdd5a8d615d0f038d0fe3339b4e5cfd5812f0c
Syam <nmsyam.mohan@gmail.com>
2026-02-01 19:10:37 +0530

    env variables corrected

--- config/envConfig.js
+++ config/envConfig.js
@@ -11,8 +11,8 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 
 // PORT=https://my-app-client-inky.vercel.app
-BASE_URL = process.env.BASE_URL;
+VITE_BASE_URL = process.env.VITE_BASE_URL;
 LOCAL_HOST = process.env.LOCAL_HOST;
 NODE_ENV = process.env.NODE_ENV;
 
-module.exports = { PORT, JWT_SECRET, JWT_EXPIRES_IN, BASE_URL, LOCAL_HOST, MONGO_URI, NODE_ENV };
+module.exports = { PORT, JWT_SECRET, JWT_EXPIRES_IN, VITE_BASE_URL, LOCAL_HOST, MONGO_URI, NODE_ENV };
